e98bf4a73928a917652dfa6a134eb57c1a45f8eb
Muneeb ur Rehman <kawaki122@gmail.com>
2019-02-05 17:39:56 +0500

    commit

--- src/App.js
+++ src/App.js
@@ -3,30 +3,24 @@ import NavBar from "./components/navBar";
 import "./App.css";
 import SideMenu from "./components/SideMenu";
 import HomeScreen from "./components/HomeScreen";
+import CenterBar from "./components/centerBar";
 
 class App extends Component {
-  state = {
-    counters: [
-      { id: 0, value: 0 },
-      { id: 1, value: 0 },
-      { id: 2, value: 0 },
-      { id: 3, value: 0 }
-    ]
-  };
-
   render() {
     return (
       <React.Fragment>
         <main className="container-fluid">
           <div className="row">
-            <div className="col-md-1">
+            <div className="col-md-3">
               <SideMenu />
             </div>
-            <div className="col-md-3" />
-            <div className="col-md-4">
-              <HomeScreen />
+            <div className="col-md-6">
+              <CenterBar />
+              <div className="border center">
+                <HomeScreen />
+              </div>
             </div>
-            <div className="col-md-4" />
+            <div className="col-md-3" />
           </div>
         </main>
       </React.Fragment>
